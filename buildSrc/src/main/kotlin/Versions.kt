2a3d66d8ec9c205f074ab876056a9bc4446a3ebb
4o4E <869951226@qq.com>
2023-05-22 21:53:45 +0800

    更新依赖版本, 移除不使用的依赖版本

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,16 +1,9 @@
 object Versions {
-    const val kotlin = "1.7.10"
+    const val kotlin = "1.8.20"
     const val spigot = "1.13.2-R0.1-SNAPSHOT"
     const val group = "top.e404"
-    const val version = "1.0.1"
-    const val kaml = "0.47.0"
+    const val version = "1.1.0"
     const val eplugin = "1.0.5"
-}
-
-object Hooks {
-    const val slimefun = "RC-30"
-    const val mmoitems = "6.7.3"
-    const val itemrepo = "1.0.0"
 }
 
 fun kotlinx(id: String, version: String) = "org.jetbrains.kotlinx:kotlinx-$id:$version"
